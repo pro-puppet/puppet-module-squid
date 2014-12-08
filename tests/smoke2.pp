e9a40456941283032719972781c59738af904cf4
Spencer Krum <nibz@cat.pdx.edu>
2014-12-07 16:28:40 -0800

    Create cache directory before using it

--- tests/smoke2.pp
+++ tests/smoke2.pp
@@ -1,3 +1,15 @@
+file { '/disk'
+  ensure => directory,
+} ->
+
+file { '/disk/trump'
+  ensure => directory,
+} ->
+
+file { '/disk/trump/squid'
+  ensure => directory,
+} ->
+
 class { 'squid':
   cache_dir => '/disk/trump/squid',
   port      => '8080',
